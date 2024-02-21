0aa67b85263176ebae92356a82ad71b7ca1d4f37
DanielMA <danoan2008@gmail.com>
2024-02-21 11:13:52 +0100

    Implement get_pos_tag

--- tests/plugins/modules/english_collins/test_english_collins.py
+++ tests/plugins/modules/english_collins/test_english_collins.py
@@ -1,5 +1,5 @@
 from danoan.word_def.plugins.modules import english_collins
-from danoan.word_def.plugins.core import exception
+from danoan.word_def.core import api, exception, model
 
 from io import StringIO
 from pathlib import Path
@@ -20,6 +20,10 @@ def test_adapter_no_config_file_error():
     with pytest.raises(exception.ConfigurationFileRequiredError) as e:
         af.get_adapter()
         assert e.type == exception.ConfigurationFileRequiredError
+
+
+def test_plugin_compatibility():
+    assert api.is_plugin_compatible(english_collins.AdapterFactory())
 
 
 def test_adapter_get_definition_handle():
@@ -52,3 +56,19 @@ def test_adapter_get_definition_handle_with_error():
         with pytest.raises(exception.UnexpectedResponseError) as e:
             list_of_definitions = adapter._get_definition_handle(mock_response)
             assert e.value.status_code == 404
+
+
+def test_adapter_get_pos_tag_handle():
+    af = english_collins.AdapterFactory()
+
+    mock_config_filepath = SCRIPT_FOLDER / "input" / "config.toml"
+    with open(mock_config_filepath, "r") as f:
+        adapter = af.get_adapter(f)
+
+    mock_response_filepath = SCRIPT_FOLDER / "input" / "legitim.json"
+    with open(mock_response_filepath, "r") as f:
+        mock_response = SimpleNamespace(text=f.read(), status_code=200)
+        list_of_pos_tags = adapter._get_pos_tag_handle(mock_response)
+
+        assert len(list_of_pos_tags) > 0
+        assert list_of_pos_tags[0] == model.PosTag.Noun
